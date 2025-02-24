701dd265987754a274360c510221bc2ad00c7225
Özgür KILIÇ <ozgurkilic147@gmail.com>
2025-02-24 17:03:47 +0100

    Added IComparable Interface.

--- CSAdvancedProg16-PredefinedInterfaces/Program.cs
+++ CSAdvancedProg16-PredefinedInterfaces/Program.cs
@@ -10,7 +10,7 @@
             //Used to compare objects.
             //It has normal and generic(type safety) versions.
             //Commonly used while ordering collections.
-            Person p1 = new Person() {Name = "Efe" , Age = 23 };
+            /*Person p1 = new Person() {Name = "Efe" , Age = 23 };
             Person p2 = new Person() {Name = "Özgür" , Age = 32 };
             Person p3 = new Person() {Name = "Kılıç" , Age = 31 };
             Person p4 = new Person() {Name = "Sabrina" , Age = 25 };
@@ -22,8 +22,27 @@
             void Printer(Person a)
             { 
                 Console.WriteLine(a.Name); 
-            }
+            }*/
+
+            #endregion
+
             
+            #region IComparable
+            //Pretty much doing the same thingie IComparer does but we are not creating an additional class containing our comparison method.
+            /*Person2 person3 = new Person2() {Age = 20, Name="Efe" };
+            Person2 person4 = new Person2() { Age = 25, Name = "Cato" };
+            switch(person3.CompareTo(person4)) {
+                case 1:
+                    Console.WriteLine("Higher");
+                    break;
+                case 0:
+                    Console.WriteLine("Equal");
+                    break;
+                case -1:
+                    Console.WriteLine("Lower");
+                    break;
+            }*/
+
             #endregion
         }
 
@@ -31,12 +50,23 @@
 
     #region IComparer
     class Person { internal string Name { get; set; } internal int Age { get; set; } }
-    class AgeComparer : IComparer<Person> { 
-        public int Compare(Person? a, Person? b) 
+    class AgeComparer : IComparer<Person>
+    {
+        public int Compare(Person? a, Person? b)
         {
             return a.Age.CompareTo(b.Age);
-        } 
+        }
     }
 
+    #endregion
+
+
+    #region IComparable
+    class Person2 : IComparable<Person2> 
+    {
+        internal string Name { get; set; }
+        internal int Age { get; set; }
+        public int CompareTo(Person2 a) { return this.Age.CompareTo(a.Age); }
+    }
     #endregion
 }
