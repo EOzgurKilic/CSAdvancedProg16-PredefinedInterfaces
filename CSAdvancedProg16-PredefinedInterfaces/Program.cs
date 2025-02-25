25499266e68d5454c8a1843674dfc4840e1cf188
orkhaner <orkhanrrahimli@gmail.com>
2025-02-25 15:40:09 +0100

    Will move on with INotifyPropertyChanged

--- CSAdvancedProg16-PredefinedInterfaces/Program.cs
+++ CSAdvancedProg16-PredefinedInterfaces/Program.cs
@@ -44,6 +44,22 @@
             }*/
 
             #endregion
+            
+            
+            #region ICloneable
+            //Noting that Prototype design pattern is recommended for cloning action rather than ICloneable Implementation as Its claimed that this interface..
+            //.. causes problems.
+            /*Person3 humanBeing1 = new Person3(){Age = 25, Name = "John Doe", Human = new Person2(){Age = 25, Name = "John Doe"}};
+            Person3 humanBeing2 = (Person3)humanBeing1.Clone();
+            humanBeing2.Age = 15; //Since Age was a value type, this change won't affect humanBeing1's same member
+            humanBeing2.Human.Name = "Efe"; //humanBeing1's Human member's Name parameter will change onto "Efe" as well.
+            Console.WriteLine(humanBeing1.Human.Name);*/
+
+            #endregion
+            
+            
+            #region INotifyPropertyChanged
+            #endregion
         }
 
     }
@@ -67,6 +83,31 @@
         internal string Name { get; set; }
         internal int Age { get; set; }
         public int CompareTo(Person2 a) { return this.Age.CompareTo(a.Age); }
+    }
+    #endregion
+    
+    
+    #region ICloneable
+    class Person3 : ICloneable
+    {
+        internal string Name { get; set; }
+        internal int Age { get; set; }
+        internal Person2 Human { get; set; }
+
+        public Object Clone()
+        {
+            //return this.MemberwiseClone(); //This is the common way returning the clone of the relevant instance.
+            //It performs a shallow copy, which is to perform deep copy of the primitive & value type members of the relevant class and..
+            //reference (shallow) the reference type members
+            //So Name and Age won't be affected but Human will be for the cloned instance just as they get changed over the clone instance.
+            
+            //Instead the method above, you can manually do the same thingie it does;
+            return new Person3() {Name = this.Name, Age = this.Age, Human = this.Human }; //Evenn this way, as you may estimate, Human of the cloned object..
+            //will be logically affected if the same member of the clone object gets changed in any aspect.
+        }
+        
+        #region INotifyPropertyChanged
+        #endregion
     }
     #endregion
 }
